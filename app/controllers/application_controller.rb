03a65a4515609676b2c22f0f1b2ee7dd856e5e59
Dave Birinyi <dave.birinyi@gmail.com>
2016-10-16 12:41:49 -0700

    Add quickie savedate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def savedate
+    render html: "Let's go to the forest... July 8, 2017"
+  end
 end
