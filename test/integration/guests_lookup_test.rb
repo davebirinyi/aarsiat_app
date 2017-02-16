d48cbc98af5309133bb5655fb7eacf16a2f8da10
Dave Birinyi <dave.birinyi@gmail.com>
2017-02-16 10:24:08 -0800

    Create dbcooper fixture for guest tests and shunt header off into re-usable shim

--- test/integration/guests_lookup_test.rb
+++ test/integration/guests_lookup_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class GuestsLookupTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @guest = guests(:dbcooper)
+  end
+
   test "lookup with invalid information" do
     get lookup_path
     assert_template 'sessions/new'
@@ -9,5 +14,16 @@ class GuestsLookupTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test "lookup with valid information" do
+    get lookup_path
+    post lookup_path, params: { session: { email: @guest.email} }
+    assert_redirected_to @guest
+    follow_redirect!
+    assert_template 'guests/show'
+    assert_select "a[href=?]", lookup_path, count: 0
+    # assert_select "a[href=?]", lookup_path
+    # assert_select "a[href=?]", guest_path(@guest)
   end
 end
