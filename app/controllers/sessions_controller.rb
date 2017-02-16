fd1e3200eeec1d5c56acea0ca45dacc8c11d5549
Dave Birinyi <dave.birinyi@gmail.com>
2017-02-16 08:51:20 -0800

    fill in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    guest = Guest.find_by(email: params[:session][:email].downcase)
+    if guest
+      # Log the guest in and redirect to guest's show page (w/ rsvp details).
+    else
+      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
   end
 
   def destroy
