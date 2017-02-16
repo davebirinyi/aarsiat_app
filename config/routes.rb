cbce10f1e790a20ba3c4eae36c0eed6502824c5f
Dave Birinyi <dave.birinyi@gmail.com>
2017-02-16 08:44:51 -0800

    generate sessions controllerand make reservation lookup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
-  root 'static_pages#savedate'
-  get  '/savedate', to: 'static_pages#savedate'
-  get  '/home',     to: 'static_pages#home'
-  get  '/rsvp',     to: 'guests#new'
-  post '/rsvp',     to: 'guests#create'
+  root   'static_pages#savedate'
+  get    '/savedate',  to: 'static_pages#savedate'
+  get    '/home',      to: 'static_pages#home'
+  get    '/rsvp',      to: 'guests#new'
+  post   '/rsvp',      to: 'guests#create'
+  get    '/lookup',    to: 'sessions#new'
+  post   '/lookup',    to: 'sessions#create'
+  delete '/logout',    to: 'sessions#destroy'
   resources :guests
 end
