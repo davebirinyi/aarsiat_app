f9495d3d56ff89756d4338f57a215862e7bb9019
Dave Birinyi <dave.birinyi@gmail.com>
2017-02-16 03:30:56 -0800

    Incorporate error handling into new guest page

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -7,4 +7,19 @@ class GuestsController < ApplicationController
   def new
     @guest = Guest.new
   end
+
+  def create
+    @guest = Guest.new(params[:guest])    # Not the final implementation!
+    if @guest.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def guest_params
+      params.require(:guest).permit(:name, :email, :saturday_adults)
+    end
 end
