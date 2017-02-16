1abb189a39d680f215c0c9f56417b4d8e87b8a13
Dave Birinyi <dave.birinyi@gmail.com>
2017-02-15 17:19:19 -0800

    Adjust controller test to new Rails Way url paths and change new guests route to rsvp url

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'guests/new'
 
   root 'static_pages#savedate'
   get  '/savedate', to: 'static_pages#savedate'
   get  '/home',     to: 'static_pages#home'
+  get  '/rsvp',     to: 'guests#new'
 end
