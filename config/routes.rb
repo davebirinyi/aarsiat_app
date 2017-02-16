57e627b339414696fe02f1f64ee1befbb85ca1ed
Dave Birinyi <dave.birinyi@gmail.com>
2017-02-15 17:03:30 -0800

    Modify routes for cleaner urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#savedate'
-  get  'static_pages/savedate'
-  get  'static_pages/home'
+  get  '/savedate', to: 'static_pages#savedate'
+  get  '/home',     to: 'static_pages#home'
 end
